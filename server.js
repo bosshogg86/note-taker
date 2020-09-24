4aa8c8cce47fbdb60c3b00c2b65888fd295d0b9a
bosshogg86 <jeffhogg86@gmail.com>
2020-09-24 00:57:59 -0700

    post req successfully writes to db.json

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-import express from "express";
-import path from "path";
-import fs from "fs";
-import { v4 as uuidv4 } from "uuid";
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const { json } = require("body-parser");
+const { v4: uuidv4 } = require("uuid");
+// import { v4 as uuidv4 } from "uuid";
 
 const app = express();
 const PORT = 3000;
 
-app.use(urlencoded({ extended: true }));
-app.use(json());
-app.use(static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 // HTML Routes
 app.get("/", function (req, res) {
@@ -21,21 +23,24 @@ app.get("/notes", function (req, res) {
 
 // Api Routes
 app.get("/api/notes", function (req, res) {
-  let rawData = fs.readFileSync("db.json");
+  let rawData = fs.readFileSync("db/db.json");
   res.send(JSON.parse(rawData));
 });
 
 app.post("/api/notes", function (req, res) {
   const newNote = req.body;
-  let rawData = fs.readFileSync("db.json");
+  let rawData = fs.readFileSync("db/db.json");
   let jsonData = JSON.parse(rawData);
   newNote.id = uuidv4();
   jsonData.push(newNote);
-  fs.writeFileSync("db.json", JSON.stringify(jsonData));
+  fs.writeFileSync("db/db.json", JSON.stringify(jsonData));
   return res.send(jsonData);
 });
 
-app.delete("/api/notes/:id", function (req, res) {});
+// app.delete("/api/notes/:id", function (req, res) {
+//   let rawData = fs.readFileSync("db.json");
+//   let jsonData = JSON.parse(rawData);
+// });
 
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
